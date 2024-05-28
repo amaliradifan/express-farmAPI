00fcf606bca2996e0bbe16f9f4042e243ca569d8
Radifan <frutami19@gmail.com>
2024-05-28 17:05:34 +0700

    update port

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use('/api', router);
+
+app.get('/', (req, res) => {
+  res.send('API Ini dah bisa dipake');
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandler);
